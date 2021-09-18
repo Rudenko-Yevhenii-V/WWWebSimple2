399cf1fcdbd5c4c0d84637cf32c7213c39b5dd27
LinuxRTTR <rydui403@mail.com>
2021-09-18 18:13:14 +0300

    first intelij

---
+++ hw/STARTER_module.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mvn -f pom.xml clean install -U
+#java -jar target/hw-1.0-SNAPSHOT.jar
---
+++ hw/src/main/java/ry/rudenko/Main.java
@@ -0,0 +1,13 @@
+package ry.rudenko;
+
+/**
+ * @author Rudenko Yevhenii
+ * @created 18/09/2021 - 6:06 PM
+ * @project hw
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Main.main");
+  }
+}
