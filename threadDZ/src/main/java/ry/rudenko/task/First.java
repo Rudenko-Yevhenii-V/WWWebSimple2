234898437a265004feb3f445457e35d357daf936
LinuxRTTR <rydui403@mail.com>
2021-10-17 16:58:52 +0300

    add First

---
+++ threadDZ/src/main/java/ry/rudenko/task/First.java
@@ -0,0 +1,42 @@
+package ry.rudenko.task;
+
+
+
+public class First {
+
+  public static void main(String[] args) {
+    System.out.println(Thread.currentThread().getName() + " RUNNING");
+    for (int i = 0; i < 50; i++) {
+
+      try {
+        String massage = "Hello from thread ";
+        new NamePrinterTask(massage + i, i).start();
+      } catch (InterruptedException e) {
+        System.out.println("Thread was interrupted");
+      }
+    }
+  }
+}
+class NamePrinterTask extends Thread {
+  int i;
+  NamePrinterTask(String name, int i) throws InterruptedException {
+    super(name);
+    this.i= i;
+    System.out.println(this.getName() + " RUNNING");
+  }
+
+  @Override
+  public void run() {
+    try {
+      Thread.sleep(100L * (50 - i));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    System.out.println("Hello from " + getName());
+  }
+
+}
+//  Напишите приложение, которое создает 50 потоков один за
+//    одним, каждый из потоков выводит сообщение "Hello from thread
+//    (number of thread)", особенность заключается в том, что вывод
+//    должен быть строго в обратном порядке, от 49 до 0
